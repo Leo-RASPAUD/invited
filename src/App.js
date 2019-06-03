e6af0216bfec117f2ee395865eb2e114714c7fce
Leo Raspaud <leo@athena.com.au>
2019-06-03 17:37:01 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import logo from './logo.svg';
+import './App.css';
+import config from './config';
+
+Amplify.configure(config);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Invite.me
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
---
+++ src/config.js
@@ -0,0 +1,13 @@
+const region = 'ap-southeast-2';
+const identityPoolId = 'ap-southeast-2:946c90ce-794b-45d2-b84a-daa126c17939';
+const userPoolId = 'ap-southeast-2_fsFLg4nP7';
+const userPoolWebClientId = '25ddkitktnc4aavk4jj7dukjm7';
+
+export default {
+  Auth: {
+    identityPoolId,
+    region,
+    userPoolId,
+    userPoolWebClientId,
+  },
+};
