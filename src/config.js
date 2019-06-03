43adc0efde9244834e1478e94d7a9e0680453901
Leo Raspaud <leo@athena.com.au>
2019-06-03 17:47:23 +1000

    Update user pool

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const region = 'ap-southeast-2';
 const identityPoolId = 'ap-southeast-2:946c90ce-794b-45d2-b84a-daa126c17939';
-const userPoolId = 'ap-southeast-2_fsFLg4nP7';
-const userPoolWebClientId = '25ddkitktnc4aavk4jj7dukjm7';
+const userPoolId = 'ap-southeast-2_VuJmvtEfB';
+const userPoolWebClientId = '6a08escb764vt5ebhqrl176mru';
 
 export default {
   Auth: {
