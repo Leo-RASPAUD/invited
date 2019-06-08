80e67dd7cf52ef528db12963e3e19966474d069c
Garth McRae <garthmcrae@gmail.com>
2019-06-08 15:04:36 +1000

    Add guests to event context

--- src/events-context.js
+++ src/events-context.js
@@ -3,6 +3,16 @@ import React from 'react';
 export const events = [
   {
     date: '6th of June',
+    guests: [
+      {
+        attending: true,
+        dietaryRequirements: 'Burgers',
+        email: 'leo@email.com',
+        name: 'Leo',
+        notes: '',
+        phone: '+6112345678',
+      }
+    ]
     host: 'Garth',
     introduction: 'Together with friends',
     location: "LP's quality meats",
