6df8771aa60f1ba2644f5119a6abb4a0bbbf66bf
Leo Raspaud <leo.raspaud@gmail.com>
2019-06-19 22:30:40 +1000

    Required

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -29,12 +29,12 @@ const Create = ({ history }) => {
             Invite from {fields.host} for a {fields.type} at {fields.place}, {fields.time} {fields.date}.
           </p>
         ) : null}
-        <Input name="name" label="Name" type="text" register={register} errors={errors} />
-        <Input name="host" label="Host" type="text" register={register} errors={errors} />
-        <Input name="type" label="Type" type="text" register={register} errors={errors} />
-        <Input name="place" label="Place" type="text" register={register} errors={errors} />
-        <Input name="time" label="Time" type="text" register={register} errors={errors} />
-        <Input name="date" label="Date" type="text" register={register} errors={errors} />
+        <Input required name="name" label="Name" type="text" register={register} errors={errors} />
+        <Input required name="host" label="Host" type="text" register={register} errors={errors} />
+        <Input required name="type" label="Type" type="text" register={register} errors={errors} />
+        <Input required name="place" label="Place" type="text" register={register} errors={errors} />
+        <Input required name="time" label="Time" type="text" register={register} errors={errors} />
+        <Input required name="date" label="Date" type="text" register={register} errors={errors} />
       </div>
       {errorMessage && <Error errorMessage={errorMessage} />}
       <input className="button" type="submit" />
