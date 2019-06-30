d0e8f0490708f4392d7593302a1f6042b930bb63
Garth McRae <garthmcrae@gmail.com>
2019-06-30 14:43:05 +1000

    Brand icon.

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-const styles = {
-  brand: {
-    color: 'inherit',
-    display: 'block',
-    fontSize: 24,
-    lineHeight: '36px',
-    textDecoration: 'none',
-  },
-};
+import { MdEvent } from 'react-icons/md';
+import styles from './Brand.module.scss';
 
 export default ({ children, ...props }) => {
   return (
-    <Link style={styles['brand']} {...props}>
-      Invited
+    <Link className={styles['brand']} {...props}>
+      <span className={styles['brand-icon']}>
+        <MdEvent />
+      </span>
+      <span className={styles['brand-text']}>{children}</span>
     </Link>
   );
 };
