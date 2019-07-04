b16edb4dd8beadadb988d8b3b4fe6f0e62a92f60
Garth <garth@192-168-1-108.tpgi.com.au>
2019-07-04 23:31:14 +1000

    Home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,33 @@ import Container from '../components/Container';
 import PageTitle from '../components/PageTitle';
 
 export default () => (
-  <Container>
-    <PageTitle>Home</PageTitle>
-  </Container>
+  <>
+    <div className="white-blue">
+      <Container>
+        <PageTitle>Hi, I'm Invited.</PageTitle>
+        <p>I'm a simple web application for organising events.</p>
+      </Container>
+    </div>
+    <div className="yellow-orange">
+      <Container>
+        <PageTitle>'How does this work?' I hear you ask.</PageTitle>
+        <p>
+          Create an account. Create an event. Invite guests. Send invitations (by email). Guests get a link and can
+          Accept or decline the invitation. You can review attendees and send out any updates if required. Easy.
+        </p>
+      </Container>
+    </div>
+    <div className="pink-black">
+      <Container>
+        <PageTitle>P.S.</PageTitle>
+        <p>
+          We are currently in <strong>alpha</strong> so things may change, break or not work as expected.
+        </p>
+        <p>
+          Be patient. Be kind. Good things come to those who wait. Shouldn't be long until we are cracking some bottles
+          at the invited <strong>launch party</strong>. although chances are you're not invited ;)
+        </p>
+      </Container>
+    </div>
+  </>
 );
