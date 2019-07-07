047ac3352a8b33de2bb3fab1780badf4136a0e16
Leo Raspaud <leo.raspaud@gmail.com>
2019-07-07 14:18:22 +1000

    Add fullstory

--- src/components/HubAuth.js
+++ src/components/HubAuth.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from 'react'; // eslint-disable-line
-import { Hub } from 'aws-amplify';
+import { Hub, Auth } from 'aws-amplify';
 import { withRouter } from 'react-router-dom';
 import UserContext from '../UserContext';
 
@@ -7,8 +7,18 @@ const HubAuth = ({ history }) => {
   const { updateCurrentUser } = useContext(UserContext);
 
   const hubAuthCallback = async ({ channel, payload: { event, data } }) => {
+    if (event === 'signIn') {
+      const user = await Auth.currentAuthenticatedUser();
+      window.FS.identify(user.attributes.sub, {
+        displayName: user.attributes.email,
+        email: user.attributes.email,
+        reviewsWritten_int: 14,
+      });
+    }
+
     if (['signIn', 'cognitoHostedUI'].includes(event)) {
       await updateCurrentUser(data);
+
       history.push('/app');
     }
     if (event === 'signOut') {
