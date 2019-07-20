17092b91a4f71222e916f86b9643cbcc174ad8bf
Garth McRae <garthmcrae@gmail.com>
2019-07-20 18:20:10 +1000

    Clip art.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,21 @@ import PageTitle from '../components/PageTitle';
 import Paragraph from '../components/Paragraph';
 import List from '../components/List';
 
+import party from '../graphics/party.svg';
+import phone from '../graphics/phone.svg';
+
+const imgStyles = {
+  height: 'auto',
+  maxWidth: '100%',
+};
+
 export default () => (
   <>
     <div className="white-blue" style={{ minHeight: '30vh' }}>
       <Container>
         <PageTitle>Hi, I'm Invited.</PageTitle>
         <Paragraph>I'm a simple web application for organising events.</Paragraph>
+        <img alt="phone" src={phone} style={imgStyles} />
       </Container>
     </div>
     <div className="yellow-teal" style={{ minHeight: '30vh' }}>
@@ -34,6 +43,7 @@ export default () => (
         <Paragraph>
           We are <strong>live</strong>.
         </Paragraph>
+        <img alt="party" src={party} style={imgStyles} />
       </Container>
     </div>
   </>
