6ad97cee907f20a0e43a248bbcc130e7b58a843e
Garth <garthmcrae@gmail.com>
2019-10-16 08:42:35 +1100

    Display participants.

--- src/components/Guest.js
+++ src/components/Guest.js
@@ -6,11 +6,16 @@ import styles from './Guest.module.scss';
 import classnames from 'classnames';
 import {} from 'react-icons/md';
 
-export default ({ firstName, lastName, email, id, accepted, notes }) => {
+export default props => {
+  const { firstName, lastName, email, id, accepted, notes, participants } = props;
   const { fetcher } = useFetcher();
   const deleteItem = () => {
     fetcher({ ...deleteGuest, params: { id } });
   };
+  function getParticipantsText(participants) {
+    const s = participants === '1' ? '' : 's';
+    return `${participants} participant${s}`;
+  }
   return (
     <div
       className={classnames(
@@ -22,6 +27,7 @@ export default ({ firstName, lastName, email, id, accepted, notes }) => {
         {firstName} {lastName}
       </h3>
       <div>{email}</div>
+      <p className={styles['notes']}>{participants ? getParticipantsText(participants) : '_'}</p>
       <p className={styles['notes']}>{notes ? notes : '_'}</p>
       <div className={styles['delete-button']}>
         <ButtonConfirm onConfirm={deleteItem}>Delete</ButtonConfirm>
